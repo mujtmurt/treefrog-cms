91c808b8407dfa782057fed12f7366d2038840f5
Muj <mujtmurt@gmail.com>
2020-02-11 11:42:18 -0500

    update

---
+++ public/app/app.js
@@ -0,0 +1,21 @@
+function initButtons() {
+  $(".get-started").click(function() {
+    $("#home div").removeClass("active");
+    $("#addNav div").addClass("active");
+
+    $(".text-wrapper").html(TREEFROG_SERVICE.getGetStartedContent());
+    $(".btn-holder").html(TREEFROG_SERVICE.getCreateNavButtons());
+  });
+
+  $("#home").click(function() {
+    $("#home div").addClass("active");
+    $("#addNav div").removeClass("active");
+
+    $(".text-wrapper").html(TREEFROG_SERVICE.getHomeContent());
+    $(".btn-holder").html(TREEFROG_SERVICE.getHomeStartButton());
+  });
+}
+
+$(document).ready(function() {
+  initButtons();
+});
